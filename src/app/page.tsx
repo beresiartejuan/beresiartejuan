b38131cd9ae9bee09bfb8ffefbb5f791c4b7811b
Juan <juanberesiarte@gmail.com>
2023-12-17 20:01:25 -0300

    Add some projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import PrimaryButton from "@/components/PrimaryButton";
 import SecondaryButton from "@/components/SecondaryButton";
 import GridIcons from "@/components/GridIcons";
 import Image from "next/image";
-import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+import { IoLogoGithub, IoLogoLinkedin, IoMdSearch } from "react-icons/io";
 import { FaLaravel, FaReact, FaSass, FaNode } from "react-icons/fa";
 import { SiTypescript, SiNextdotjs, SiMysql, SiMongodb, SiSymfony } from "react-icons/si";
+import Link from "next/link";
+import GitCard from "@/components/GitCard";
 
 export default function Home() {
+
   return (
-    <Center className="mt-[20vh] mb-[5vh] flex flex-col gap-8">
+    <Center className="mt-[10vh] mb-[5vh] flex flex-col gap-8">
       <Image
         src={"/profile.jpg"}
         alt="profile picture"
@@ -32,11 +35,15 @@ export default function Home() {
       </section>
       <hr />
       <section className="flex flex-row flex-wrap gap-2">
-        <Subtitle className="text-3xl font-bold">About me</Subtitle>
+        <Subtitle>About me</Subtitle>
         <p>Web developer with experience in <span className="bg-yellow-800">Javascript and PHP</span></p>
       </section>
+      <section className="flex flex-col gap-3">
+        <SecondaryButton className="">📂 Do u wanna see my blog?</SecondaryButton>
+        <Link href="/#projects"><span className="underline">Or see my projects?</span> 👈</Link>
+      </section>
       <section className="flex flex-col flex-wrap gap-7 mt-[5vh]">
-        <Subtitle className="text-3xl font-bold">Tech Stack</Subtitle>
+        <Subtitle>Tech Stack</Subtitle>
         <GridIcons>
           <FaLaravel className="text-red-800"></FaLaravel>
           <FaReact className="text-sky-800"></FaReact>
@@ -48,6 +55,26 @@ export default function Home() {
           <SiMysql className="text-blue-500"></SiMysql>
           <SiMongodb className="text-green-600"></SiMongodb>
         </GridIcons>
+      </section>
+      <section className="flex flex-col flex-wrap gap-7 mt-[5vh]" id="projects">
+        <Subtitle>Projects</Subtitle>
+        <div className="flex flex-col gap-3">
+          <GitCard
+            title="Typing-check"
+            description="Website to check your typing speed"
+            href="https://github.com/beresiartejuan/typing-check"
+          ><span className="block mt-2 text-xs">🟡 Javascript</span></GitCard>
+          <GitCard
+            title="Blank"
+            description="Simple and useful to-do list"
+            href="https://github.com/beresiartejuan/Blank"
+          ><span className="block mt-2 text-xs">🟡 Javascript</span></GitCard>
+          <GitCard
+            title="I love penguins"
+            description="A landing page about penguins"
+            href="https://github.com/beresiartejuan/ilovepenguins"
+          ><span className="block mt-2 text-xs">🔵 Php</span></GitCard>
+        </div>
       </section>
     </Center>
   );
