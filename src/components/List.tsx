7d2e30bcb84eae911ffd4a0b28af85de3d5c8b50
juan beresiarte <juanberesiarte@gmail.com>
2025-07-09 21:39:40 -0300

    delete all

--- src/components/List.tsx
+++
@@ -1,9 +0,0 @@
-export default function List({
-    children
-}: {
-    children: React.ReactNode,
-}) {
-    return (<ul className="flex flex-col flex-wrap gap-2 mt-5">
-        {children}
-    </ul>)
-}
